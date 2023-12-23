e3f6f0e48bc780a8212702586eadc62fff1ee8d3
JSantana11b <133810363+JSantana11b@users.noreply.github.com>
2023-12-23 12:26:37 -0500

    applies updates to meet requirements

--- src/Graph.tsx
+++ src/Graph.tsx
@@ -14,7 +14,7 @@ interface IProps {
  * Perspective library adds load to HTMLElement prototype.
  * This interface acts as a wrapper for Typescript compiler.
  */
-interface PerspectiveViewerElement {
+interface PerspectiveViewerElement extends HTMLElement {
   load: (table: Table) => void,
 }
 
@@ -32,7 +32,24 @@ class Graph extends Component<IProps, {}> {
 
   componentDidMount() {
     // Get element to attach the table from the DOM.
-    const elem: PerspectiveViewerElement = document.getElementsByTagName('perspective-viewer')[0] as unknown as PerspectiveViewerElement;
+    const elem = document.getElementsByTagName('perspective-viewer')[0] as unknown as PerspectiveViewerElement;
+
+    elem.setAttribute('view', 'y_line');
+    elem.setAttribute('column-pivots', '["stock"]');
+    elem.setAttribute('row-pivots', '["timestamp"]');
+    elem.setAttribute('columns', '["top_ask_price"]');
+    elem.setAttribute('aggregates', `
+    {"stock":"distinct count",
+    "top_ask_price":"avg",
+    "top_bid_price":"avg",
+    "timestamp":"distinct count"}`);
+    // elem.setAttribute('filters', '[["stock", "==", "AAPL"]]');
+
+
+    // Define the schema for the table.
+    // This is similar to defining the schema for a SQL table.
+    // You can learn more about Perspective schemas here:
+    // https://perspective.finos.org/docs/md/schemas.html
 
     const schema = {
       stock: 'string',
